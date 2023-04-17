eac0af533b358b0a4bedd461293b5b8877b46ea1
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2023-04-16 23:57:39 -0300

    adding search field

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { useState } from 'react'
-import './App.css'
 import Cardapio from "./pages/Cardapio";
 
 function App() {
--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,17 +1,29 @@
 import styles from './Cardapio.module.scss'
 import logo from '../../assets/logo.svg'
+import Buscador from "./Buscador";
+import {useState} from "react";
 
 export default function Cardapio() {
+    const [busca, setBusca] = useState("");
+
     return (
         <main>
-            <nav className={styles.menu}>
-                <img src={logo} alt="Logo Aluroni" />
-            </nav>
+            <div className="container">
+                <nav className={styles.menu}>
+                    <img src={logo} alt="Logo Aluroni" />
+                </nav>
+            </div>
             <header className={styles.header}>
                 <div className={styles.header__text}>
                     A casa do código e da massa
                 </div>
             </header>
+            <div className="container">
+                <section className={styles.cardapio}>
+                    <h3 className={styles.cardapio__title}>Cardápio</h3>
+                    <Buscador busca={busca} setBusca={setBusca} />
+                </section>
+            </div>
         </main>
     )
 }
